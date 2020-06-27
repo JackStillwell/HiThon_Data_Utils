8071c646188529ab9feb831ea88f9953de0badf6
Jack Stillwell <jack.stillwell@outlook.com>
2020-06-27 13:59:45 -0400

    added a "quick info" method

--- hithon_data_utils/__init__.py
+++ hithon_data_utils/__init__.py
@@ -7,7 +7,29 @@ from tqdm import tqdm
 from typing import Dict, Union, List, Tuple, Any
 
 GodMatchData = Dict[str, List[Dict[str, Any]]]
-Id_Name = namedtuple('Id_Name', ['id', 'name'])
+Id_Name = namedtuple("Id_Name", ["id", "name"])
+InfoReturn = namedtuple("InfoReturn", ["most_played", "most_banned", "most_picked"])
+
+
+def get_queue_info(datapath: str, queue: str) -> InfoReturn:
+    god_map = load_god_map(os.path.join(datapath, "gods.json"))
+    conquest_data = load_from_directory(os.path.join(datapath, queue + "_match_data"))
+    filtered_conquest_data = filter_by_field(
+        conquest_data, "conquest_tier", list(range(15, 31))
+    )
+    most_played_conquest = convert_keys_to_names(
+        get_most_played(filtered_conquest_data), god_map
+    )
+    most_banned_conquest = convert_keys_to_names(
+        get_most_banned(filtered_conquest_data), god_map
+    )
+    most_picked_conquest = convert_keys_to_names(
+        get_most_picked(filtered_conquest_data), god_map
+    )
+
+    return InfoReturn(
+        most_played_conquest[:10], most_banned_conquest[:10], most_picked_conquest[:10],
+    )
 
 
 def load_from_directory(directory_path: str) -> GodMatchData:
@@ -23,8 +45,7 @@ def load_from_directory(directory_path: str) -> GodMatchData:
 
 def save_to_directory(data: GodMatchData, directory_path: str):
     for god_id in tqdm(data.keys()):
-        with open(os.path.join(directory_path, f"{god_id}.json"),
-                  "w") as file_pointer:
+        with open(os.path.join(directory_path, f"{god_id}.json"), "w") as file_pointer:
             str_to_write = json.dumps(data[god_id])
             file_pointer.write(str_to_write)
 
@@ -40,17 +61,16 @@ def load_item_map(file_path: str) -> List[Id_Name]:
     with open(file_path, "r") as in_file:
         raw_data = json.loads(in_file.read())
 
-    return [
-        Id_Name(entry["ItemId"], entry["DeviceName"]) for entry in raw_data
-    ]
+    return [Id_Name(entry["ItemId"], entry["DeviceName"]) for entry in raw_data]
 
 
-def filter_by_field(data: GodMatchData, field_name: str,
-                    accepted_values: List[Union[str, int, float]]
-                    ) -> GodMatchData:
+def filter_by_field(
+    data: GodMatchData, field_name: str, accepted_values: List[Union[str, int, float]]
+) -> GodMatchData:
     return {
         key: [
-            match_data for match_data in val
+            match_data
+            for match_data in val
             if match_data[field_name] in accepted_values
         ]
         for key, val in data.items()
@@ -58,9 +78,10 @@ def filter_by_field(data: GodMatchData, field_name: str,
 
 
 def get_most_played(data: GodMatchData) -> List[Tuple[str, int]]:
-    ret_obj = [(name, num_matches)
-               for name, num_matches in [(key, len(val))
-                                         for key, val in data.items()]]
+    ret_obj = [
+        (name, num_matches)
+        for name, num_matches in [(key, len(val)) for key, val in data.items()]
+    ]
     ret_obj.sort(key=lambda x: x[1], reverse=True)
     return ret_obj
 
@@ -69,7 +90,7 @@ def get_most_banned(data: GodMatchData) -> List[Tuple[str, int]]:
     ret_obj = {}
     for god in data.keys():
         for match in data[god]:
-            for ban in match["BanIds"]:
+            for ban in match["ban_ids"]:
                 if ban in ret_obj:
                     ret_obj[ban] += 1
                 else:
@@ -101,11 +122,13 @@ def get_most_picked(data: GodMatchData) -> List[Tuple[str, int]]:
     return retobj
 
 
-def get_id_name(id_or_name: Union[int, str],
-                mapping: List[Id_Name]) -> Id_Name:
+def get_id_name(id_or_name: Union[int, str], mapping: List[Id_Name]) -> Id_Name:
     return next(
-        (entry for entry in mapping if str(id_or_name) == str(entry[0])
-         or str(id_or_name) == str(entry[1])),
+        (
+            entry
+            for entry in mapping
+            if str(id_or_name) == str(entry[0]) or str(id_or_name) == str(entry[1])
+        ),
         Id_Name(-1, "NOT FOUND"),
     )
 
@@ -115,54 +138,25 @@ def get_most_recent_match_id(data: GodMatchData) -> str:
     for v in data.values():
         all_matches.extend(v)
 
-    all_matches.sort(key=lambda x: x["Match_Date_Timestamp"], reverse=True)
-    return all_matches[0]["MatchId"]
+    all_matches.sort(key=lambda x: x["match_date_timestamp"], reverse=True)
+    return all_matches[0]["match_id"]
 
 
-def migrate_records_in_directory(directory_path: str, ):
-    data = load_from_directory(directory_path)
-    all_data = [match for matches in data.values() for match in matches]
-
-    # first build a dict of all gods in matches
-    match_gods: Dict[int, Dict[int, List[int]]] = {}
-    for match in tqdm(all_data):
-        if not match_gods.get(match["MatchId"], None):
-            match_gods[match["MatchId"]] = {1: [], 2: []}
-
-        match_gods[match["MatchId"]][match["TaskForce"]].append(match["GodId"])
-
-    for match in tqdm(all_data):
-        opposing_taskforce = 1 if match["TaskForce"] == 2 else 2
-
-        match["Allied_GodIds"] = [v for v in match_gods[match["MatchId"]][
-            match["TaskForce"]] if v != match["GodId"]]
-        match["Opposing_GodIds"] = match_gods[
-            match["MatchId"]][opposing_taskforce]
-
-    ret_gmd = {}
-    # build the final dictionary
-    for mod_data in all_data:
-        if not ret_gmd.get(mod_data["GodId"], None):
-            ret_gmd[mod_data["GodId"]] = []
-
-        ret_gmd[mod_data["GodId"]].append(mod_data)
-
-    save_to_directory(ret_gmd, directory_path)
-
-
-def convert_keys_to_names(data: List[Tuple[str, Any]],
-                          god_map: List[Id_Name]) -> List[Tuple[str, Any]]:
+def convert_keys_to_names(
+    data: List[Tuple[str, Any]], god_map: List[Id_Name]
+) -> List[Tuple[str, Any]]:
     return [(get_id_name(k, god_map).name, v) for k, v in data]
 
 
-WinningData = namedtuple('WinningData', ['num_wins', 'perc_wins'])
+WinningData = namedtuple("WinningData", ["num_wins", "perc_wins"])
 
 
 def get_most_winning(data: GodMatchData) -> List[Tuple[str, WinningData]]:
     win_data = {
         key: WinningData(
-            len([v for v in val if v['Win_Status'] == 'Winner']),
-            len([v for v in val if v['Win_Status'] == 'Winner']) / len(val))
+            len([v for v in val if v["win_status"] == "Winner"]),
+            len([v for v in val if v["win_status"] == "Winner"]) / len(val),
+        )
         for key, val in data.items()
     }
 
@@ -171,18 +165,27 @@ def get_most_winning(data: GodMatchData) -> List[Tuple[str, WinningData]]:
     return ret_list
 
 
-def get_most_winning_against(god_id: str, data: GodMatchData
-                             ) -> List[Tuple[str, WinningData]]:
+def get_most_winning_against(
+    god_id: str, data: GodMatchData
+) -> List[Tuple[str, WinningData]]:
     win_data = {
         key: WinningData(
-            len([
-                v for v in val if v['Win_Status'] == 'Winner'
-                and god_id in v['Opposing_GodIds']
-            ]),
-            len([
-                v for v in val if v['Win_Status'] == 'Winner'
-                and god_id in v['Opposing_GodIds']
-            ]) / len(val))
+            len(
+                [
+                    v
+                    for v in val
+                    if v["win_status"] == "Winner" and god_id in v["opposing_god_ids"]
+                ]
+            ),
+            len(
+                [
+                    v
+                    for v in val
+                    if v["win_status"] == "Winner" and god_id in v["opposing_god_ids"]
+                ]
+            )
+            / len(val),
+        )
         for key, val in data.items()
     }
 
